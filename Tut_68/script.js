c708d126879d42b6924bc7edc564287f46d66b5b
KishorLangote <kishorlangote2@gmail.com>
2024-01-17 19:18:50 +0530

    practice

---
+++ Tut_68/script.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+
+let boxes = document.getElementsByClassName("box");
+console.log(boxes);
+boxes[3].style.backgroundColor = "red";
+boxes[0].style.backgroundColor = "green";
+boxes[1].style.backgroundColor = "pink";
+boxes[2].style.backgroundColor = "purple";
+boxes[4].style.backgroundColor = "gray";
+
+let id = document.getElementById("john");
+console.log(id);
+id.style.backgroundColor = "gray";  
