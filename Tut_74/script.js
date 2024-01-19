6737fa3858c7f8ab7fb3a760868ae40e16a354be
KishorLangote <kishorlangote2@gmail.com>
2024-01-19 11:38:42 +0530

    event, event bubbling, set timeout and set interval

---
+++ Tut_74/script.js
@@ -0,0 +1,26 @@
+// list of all events : https://developer.mozilla.org/en-US/docs/Web/API/Event
+
+let button = document.getElementById("btn")
+
+// event add, remove :
+
+// button.addEventListener("dblclick", () => {
+//     alert("Hey you are clicked");
+// })
+
+
+// button.addEventListener("click", () =>{
+//         // alert("Hey you are clicked");
+//         document.querySelector(".box").innerHTML = "Now you have clicked and then has been changed the content between the your div <b> By kishor </b>";
+        
+//     })
+
+// setTimeout , setInterval, clearTimeout : 
+
+setInterval(() => {
+    document.querySelector("btn") = alert("Hey you are clicked");
+}, 5000);
+
+setTimeout(() => {
+    
+}, 5000);
